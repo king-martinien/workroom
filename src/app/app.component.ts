0768faea8b32b332f93db427ca861cbce0699fa7
king-martinien <martinienfokoue@yahoo.com>
2025-03-03 07:28:18 +0100

    core: tailwindcss installation & configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'workroom-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-}
+export class AppComponent {}
